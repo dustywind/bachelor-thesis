01eef0271051ae1038a4295fafefd419085bd684
VCharlie <victorcharlie@web.de>
2015-05-19 13:20:12 +0200

    initial commit

---
+++ recommender_system_impl/information-retrieval/productimport.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+__author__ = 'dust'
+
+import codecs
+
+from productinfo import Bluse
+
+
+def main():
+    f = codecs.open('./foobar.txt', 'r', 'utf-8')
+    f.readline() #skip first line
+    s = f.readline()
+    print s
+    b = Bluse(s)
+    print b
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ recommender_system_impl/information-retrieval/productinfo.py
@@ -0,0 +1,78 @@
+__author__ = 'dust'
+
+import re
+
+class Bluse:
+    #__input_regex = re.compile('(http://i[12]\.ztat\.net[a-zA-Z\/0-9-@]*(?:\.[0-9]\.jpg|\.jpg)) (?:([a-zA-Z&\xfc\. ]*) - ([a-zA-Z ]*)|([a-zA-Z&\xfc\.\s]*) (Bluse))\s-\s([a-zA-Z\xdf\-\/ ]*) ([0-9, ]*) \u20ac ? (?:[0-9]{2}(?: cm) ){1,2}bei Gr\xf6\xdfe (?:(?:EU )?[0-9]{1,2}|[A-Z]{1,3}) ([a-zA-Z -]*) ((?:[0-9]{1,3}% [a-zA-Z]*(?:, )?)+)')
+    __input_regex = re.compile(u'(http://i[12]\.ztat\.net[a-zA-Z\/0-9-@]*(?:\.[0-9]\.jpg|\.jpg)) (?:([a-zA-Z&\u00fc\. ]*) - ([a-zA-Z ]*)|([a-zA-Z&\u00fc\.\s]*) (Bluse))\s-\s([a-zA-Z\u00df\-\/ ]*) ([0-9, ]*) \u20ac ? (?:[0-9]{2}(?: cm) ){1,2}bei Gr\u00f6\u00dfe (?:(?:EU )?[0-9]{1,2}|[A-Z]{1,3}) ([a-zA-Z -]*) ((?:[0-9]{1,3}% [a-zA-Z]*(?:, )?)+)')
+
+    def __init__(self, description):
+        self.__description = description
+        match = Bluse.__input_regex.match(self.__description)
+
+        if not match:
+            raise ValueError(description)
+
+        self._image_name = self._get_image_name_from_match(match)
+        self._brand = self._get_brand_from_match(match)
+        self._cloth_type = self._get_cloth_type_from_match(match)
+        self._colours = self._get_colours_from_match(match)
+        self._price = self._get_price_from_match(match)
+        self._collar_type = self._get_collar_type_from_match(match)
+        self._components = self._get_components_from_match(match)
+        pass
+
+    def _get_image_name_from_match(self, match):
+        url = match.group(1)
+        url_parts = url.split('/')
+        return url_parts[-1]
+        
+    def _get_brand_from_match(self, match):
+        if not match.group(2):
+            return match.group(4)
+        else:
+            return match.group(2)
+
+    def _get_cloth_type_from_match(self, match):
+        if not match.group(3):
+            return match.group(5)
+        else:
+            return match.group(3)
+
+    def _get_colours_from_match(self, match):
+        colours = match.group(6)
+        return colours.split('/')
+
+    def _get_price_from_match(self, match):
+        return match.group(7)
+
+    def _get_collar_type_from_match(self, match):
+        return match.group(8)
+    
+    def _get_components_from_match(self, match):
+        return match.group(7)
+
+    def get_image_name(self):
+        return self._image_name
+
+    def get_brand(self):
+        return self._brand
+
+    def get_cloth_type(self):
+        return self._cloth_type
+
+    def get_colours(self):
+        return self._colours
+
+    def get_price(self):
+        return self._price
+
+    def get_collar_type(self):
+        return self._collar_type
+
+    def get_components(self):
+        return self._components
+
+
+    def __str__(self):
+        return '%s, %s, %s, %s, %s, %s, %s' % (self._image_name, self._brand, self._cloth_type, self._colours, self._price, self._collar_type, self._components)

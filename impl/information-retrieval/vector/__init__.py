d69760ab1d00f7ac8cf3d23c8b8ffcd6521ae3c6
Dustin Wind <dusty@posteo.de>
2015-05-26 21:18:03 +0200

    added forgotten file

---
+++ impl/information-retrieval/vector/__init__.py
@@ -0,0 +1,14 @@
+
+
+from vectormanager import VectorManager
+from clothingvectormanager import ClothingVectorManager
+
+from vectortablecreator import VectorTableCreator
+
+from tfidfvectorcreator import TfIdfVectorCreator
+from vectorcreatorfabric import VectorCreatorFabric
+
+from tfidfvector import TfIdfVector
+from documentvector import DocumentVector
+
+

415991d33205c82af88224f1bfe33eba1737d8ec
Dusty Wind <dusty@posteo.de>
2015-08-31 12:21:30 +0200

    changed productimport. There wont be any preselection of terms. Instead all terms will be imported

--- impl/recommender/product/__init__.py
+++ impl/recommender/product/__init__.py
@@ -1,5 +1,6 @@
 
 from .productcreator import ProductCreator
+from .nproductcreator import NProductCreator
 from .productmanager import ProductManager
 from .product import Product
 from .producttablecreator import ProductTableCreator
